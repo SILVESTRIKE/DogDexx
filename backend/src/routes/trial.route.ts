c590ee74acdac465f2b14830a9925d9b2162d375
DUONG VAN <118115054+SILVESTRIKE@users.noreply.github.com>
2025-10-14 07:54:44 +0700

    feat(project): clean up repo

--- backend/src/routes/trial.route.ts
+++
@@ -1,9 +0,0 @@
-
-import { Router } from 'express';
-import { trialController } from '../controllers/trial.controller';
-
-const router = Router();
-
-router.post('/api/trial/start', trialController.startTrial);
-
-export { router as trialRouter };
