104e047ec69dba3e9143a7997b8efa242e232ad9
DUONG VAN <118115054+SILVESTRIKE@users.noreply.github.com>
2025-11-18 09:59:40 +0700

    Update redis.util.ts

--- backend/src/utils/redis.util.ts
+++ backend/src/utils/redis.util.ts
@@ -1,29 +1,30 @@
-import { createClient } from "redis";
-import { logger } from "./logger.util";
+import { createClient } from 'redis';
+import { logger } from './logger.util';
 
 const redisUrl = process.env.REDIS_URL as string;
 
 let client: ReturnType<typeof createClient> | null = null;
 
 if (!redisUrl) {
-  logger.warn("REDIS_URL is not defined. Guest token limiter will not work.");
+  logger.warn('REDIS_URL is not defined. Guest token limiter will not work.');
 } else {
   client = createClient({
     url: redisUrl,
-    
   });
 
-  client.on("connect", () => {});
-
-  client.on("ready", () => {
-    logger.info("[Redis] Connection on " + redisUrl);
+  client.on('connect', () => {
   });
 
-  client.on("error", (err) => {
-    logger.error("[Redis] Redis Client Error", err);
+  client.on('ready', () => {
+    logger.info('[Redis] Connection on ' + redisUrl);
+  });
+
+  client.on('error', (err) => {
+    logger.error('[Redis] Redis Client Error', err);
   });
 
   client.connect();
+
 }
 
 export const redisClient = client;
