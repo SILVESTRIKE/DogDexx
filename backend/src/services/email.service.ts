fdbf36ca9f70e3e721a450909b9a95a5bbbafa2a
DUONG VAN <118115054+SILVESTRIKE@users.noreply.github.com>
2025-11-24 21:01:12 +0700

    Update email.service.ts

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -4,15 +4,15 @@ import { logger } from "../utils/logger.util";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: parseInt(process.env.EMAIL_PORT || "587", 10),
-  secure: (process.env.EMAIL_PORT === '465'), // true for 465, false for other ports
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
-
 async function sendGenericEmail(
   to: string,
   subject: string,
